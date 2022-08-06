10602fff037dfb8ad20a8e341aba2a4bd40e7737
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-08-06 16:03:30 +0530

    -Implemented custom logging

--- main.go
+++ main.go
@@ -5,12 +5,40 @@ import (
 	"os"
 )
 
-func main() {
-	file, err := os.OpenFile("logs", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+var (
+	InfoLogger    *log.Logger
+	WarningLogger *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING:", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR:", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func CustomLogger() {
+	InfoLogger.Println("Application")
+	WarningLogger.Println("Something might go wrong")
+	ErrorLogger.Println("Something wrong")
+}
+
+func SimpleLogger() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.SetOutput(file)
 	log.Println("Log Info")
+}
+
+func main() {
+	// SimpleLogger()
+	CustomLogger()
 }
