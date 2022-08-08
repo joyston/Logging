a73a7c4de0e5bf22818297878078958ae02a0d65
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-08-08 23:22:31 +0530

    -Added more functions and fixed the output

--- logrus.go
+++ logrus.go
@@ -12,7 +12,19 @@ func ExecuteLogrus() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Warning("Just a warning")
 	log.SetOutput(file)
+	log.Warning("Just a warning")
+
+	log.Trace("Something very low level.")
+	// log.SetLevel(log.DebugLevel)
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+
+	// Calls os.Exit(1) after logging
+	log.Fatal("Bye.")
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+
 }
